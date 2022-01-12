49baa7e8984cfb2db673dd96252d9fcd531ea841
Marcin Orlowski <marcin.orlowski@develart.cz>
2022-01-12 16:03:29 +0100

    Included extra assert helpers

--- tests/BaseTestCase.php
+++ tests/BaseTestCase.php
@@ -12,8 +12,9 @@ namespace OlzaLogistic\PpApi\Client\Tests;
  * @license   http://www.opensource.org/licenses/mit-license.php MIT
  * @link      https://github.com/develart-projects/pickup-points-api-client/
  */
-
 abstract class BaseTestCase extends \PHPUnit\Framework\TestCase
 {
     use Traits\TestHelpersTrait;
+    use Traits\AccessHelpersTrait;
+    use \MarcinOrlowski\PhpunitExtraAsserts\Traits\ExtraAsserts;
 } // end of class
