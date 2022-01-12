df53f4fdc76a33928ad27ccc57384f46ae7a483b
Marcin Orlowski <marcin.orlowski@develart.cz>
2022-01-12 16:06:06 +0100

    Extended Params class tests to cover location parameter

---
+++ tests/Client/ParamsTest.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace OlzaLogistic\PpApi\Client\Tests\Model;
+
+/**
+ * Olza Logistic's Pickup Points API client
+ *
+ * @package   OlzaLogistic\PpApi\Client
+ *
+ * @author    Marcin Orlowski <mail (#) marcinOrlowski (.) com>
+ * @copyright 2021 DevelArt IV
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT
+ * @link      https://github.com/develart-projects/pickup-points-api-client/
+ */
+
+use OlzaLogistic\PpApi\Client\Params;
+use OlzaLogistic\PpApi\Client\Tests\BaseTestCase;
+use OlzaLogistic\PpApi\Client\Tests\Location;
+
+class ParamsTest extends BaseTestCase
+{
+    public function testCreate(): void
+    {
+        $p = Params::create();
+        $this->assertInstanceOf(Params::class, $p);
+    }
+
+    public function testWithAccessToken(): void
+    {
+        $accessToken = $this->getRandomString('token');
+        $p = Params::create()->withAccessToken($accessToken);
+        $value = $this->callProtectedMethod($p, 'getAccessToken');
+        $this->assertIsString($value);
+        $this->assertEquals($accessToken, $value);
+    }
+
+    public function testCountry(): void
+    {
+        $expected = $this->getRandomString('country');
+        $p = Params::create()->withCountry($expected);
+        $value = $this->callProtectedMethod($p, 'getCountry');
+        $this->assertIsString($value);
+        $this->assertEquals($expected, $value);
+    }
+
+    public function testCity(): void
+    {
+        $expected = $this->getRandomString('city');
+        $p = Params::create()->withCity($expected);
+        $value = $this->callProtectedMethod($p, 'getCity');
+        $this->assertIsString($value);
+        $this->assertEquals($expected, $value);
+    }
+
+    public function testSpedition(): void
+    {
+        $expected = $this->getRandomString('spedition');
+        $p = Params::create()->withSpedition($expected);
+        $value = $this->callProtectedMethod($p, 'getSpedition');
+        $this->assertIsString($value);
+        $this->assertEquals($expected, $value);
+    }
+
+    public function testSpeditionId(): void
+    {
+        $expected = $this->getRandomString('speditionId');
+        $p = Params::create()->withSpeditionId($expected);
+        $value = $this->callProtectedMethod($p, 'getSpeditionId');
+        $this->assertIsString($value);
+        $this->assertEquals($expected, $value);
+    }
+
+    public function testLocation(): void
+    {
+        $latitude = Location::getRandomLongitude();
+        $longitude = Location::getRandomLatitude();
+        $p = Params::create()->withLocation($latitude, $longitude);
+        $value = $this->callProtectedMethod($p, 'getLatitude');
+        $this->assertIsFloat($value);
+        $this->assertEquals($latitude, $value);
+        $value = $this->callProtectedMethod($p, 'getLongitude');
+        $this->assertIsFloat($value);
+        $this->assertEquals($longitude, $value);
+    }
+
+    public function testWithFields(): void
+    {
+        $fields = [];
+        $cnt = $this->getRandomInt(5, 25);
+        for ($i = 0; $i < $cnt; $i++) {
+            $fields[] = $this->getRandomString("field_{$i}");
+        }
+        $p = Params::create()->withFields($fields);
+        $value = $this->callProtectedMethod($p, 'getFields');
+        $this->assertArrayEquals($fields, $value);
+    }
+
+    public function testGetFieldsAsString(): void
+    {
+        $fields = [];
+        $cnt = $this->getRandomInt(5, 25);
+        for ($i = 0; $i < $cnt; $i++) {
+            $fields[] = $this->getRandomString("field_{$i}");
+        }
+        $fieldsAsString = \implode(',', $fields);
+
+        $p = Params::create()->withFields($fields);
+        $value = $this->callProtectedMethod($p, 'getFieldsAsString');
+        $this->assertEquals($fieldsAsString, $value);
+    }
+
+    public function testAddField(): void
+    {
+        $fields = [];
+        $p = Params::create();
+
+        $cnt = $this->getRandomInt(5, 25);
+        for ($i = 0; $i < $cnt; $i++) {
+            $field = $this->getRandomString("field_{$i}");
+            $p->addField($field);
+            $fields[] = $field;
+        }
+        $value = $this->callProtectedMethod($p, 'getFields');
+        $this->assertArrayEquals($fields, $value);
+    }
+
+} // end of class
