a9549e5eb914131673239f5b1bae544c078de14b
Marcin Orlowski <marcin.orlowski@develart.cz>
2022-02-08 16:02:14 +0100

    Added parsing exceptions support.

---
+++ src/Exception/ResponseParsingException.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+namespace OlzaLogistic\PpApi\Client\Exception;
+
+/**
+ * Olza Logistic's Pickup Points API (server)
+ *
+ * @package   OlzaLogistic\PpApi\Server
+ *
+ * @author    Marcin Orlowski <mail (#) marcinOrlowski (.) com>
+ * @copyright 2022 DevelArt
+ * @license   Proprietary
+ */
+
+class ResponseParsingException extends \RuntimeException
+{
+    public function __construct(?string $msg = null, int $code = 0, Throwable $previous = null)
+    {
+        $msg ??= "Failed parsing API response.";
+        parent::__construct($msg, $code, $previous);
+    }
+
+} // end of class
