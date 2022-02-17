a4896ce979ff71ce78cf709c1478e7257e2aa106
Marcin Orlowski <marcin.orlowski@develart.cz>
2022-02-17 21:34:46 +0100

    Updated Country class consts

--- src/Country.php
+++ src/Country.php
@@ -15,12 +15,29 @@ namespace OlzaLogistic\PpApi\Client;
 class Country
 {
     /** @var string */
+    public const BULGARIA = 'bg';
+    /** @var string */
     public const CZECH_REPUBLIC = 'cz';
     /** @var string */
-    public const SLOVAKIA = 'sk';
+    public const GREECE = 'gr';
     /** @var string */
     public const HUNGARY = 'hu';
     /** @var string */
     public const POLAND = 'pl';
+    /** @var string */
+    public const ROMANIA = 'ro';
+    /** @var string */
+    public const SLOVAKIA = 'sk';
+
+    /** ********************************************************************************************* **/
+
+    /** @var float */
+    public const LAT_MIN = -90.0;
+    /** @var float */
+    public const LAT_MAX = 90.0;
+    /** @var float */
+    public const LONG_MIN = -180.0;
+    /** @var float */
+    public const LONG_MAX = 180.0;
 
 } // end of class
