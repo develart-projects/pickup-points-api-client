e2a01a8e1ef526401e6b710f8a1e067bebd8d8b3
Marcin Orlowski <MarcinOrlowski@users.noreply.github.com>
2023-07-03 12:26:11 +0200

    Updated copyright years

--- src/ConfigData.php
+++ src/ConfigData.php
@@ -10,7 +10,7 @@ use OlzaLogistic\PpApi\Client\Model\Spedition;
  * @package   OlzaLogistic\PpApi\Client
  *
  * @author    Marcin Orlowski <marcin.orlowski (#) develart (.) cz>
- * @copyright 2021-2022 DevelArt
+ * @copyright 2021-2023 DevelArt
  * @license   http://www.opensource.org/licenses/mit-license.php MIT
  * @link      https://github.com/develart-projects/pickup-points-api-client/
  */
--- tests/Util/Location.php
+++ tests/Util/Location.php
@@ -8,7 +8,7 @@ namespace OlzaLogistic\PpApi\Client\Tests\Util;
  * @package   OlzaLogistic\PpApi\Client
  *
  * @author    Marcin Orlowski <marcin.orlowski (#) develart (.) cz>
- * @copyright 2021-2022 DevelArt
+ * @copyright 2021-2023 DevelArt
  * @license   http://www.opensource.org/licenses/mit-license.php MIT
  * @link      https://github.com/develart-projects/pickup-points-api-client/
  */
