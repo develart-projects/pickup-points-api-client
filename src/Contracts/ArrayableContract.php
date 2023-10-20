a859a66dd52436798d2185b015a01819c5d23dda
Marcin Orlowski <MarcinOrlowski@users.noreply.github.com>
2023-10-20 08:42:56 +0200

    Added Arrayable contract

---
+++ src/Contracts/ArrayableContract.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+/*
+ * Olza Logistic's Pickup Points API client
+ *
+ * @author    Marcin Orlowski <marcin.orlowski (#) develart (.) cz>
+ * @copyright 2021-2023 DevelArt
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT
+ * @link      https://github.com/develart-projects/pickup-points-api-client/
+ */
+
+namespace OlzaLogistic\PpApi\Client\Contracts;
+
+interface ArrayableContract
+{
+    /**
+     * Returns array representation of the object.
+     */
+    public function toArray(): array;
+
+} // end of class
