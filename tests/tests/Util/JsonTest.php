080b04d3abc7d077ef39add7c4e42104070c6c3f
Marcin Orlowski <MarcinOrlowski@users.noreply.github.com>
2024-07-09 23:01:01 +0200

    Added more JSON helper tests

--- tests/tests/Util/JsonTest.php
+++ tests/tests/Util/JsonTest.php
@@ -42,4 +42,74 @@ class JsonTest extends BaseTestCase
         Json::decode($str);
     }
 
+    /* ****************************************************************************************** */
+
+    /**
+     * Tests successful encoding of an array to JSON.
+     */
+    public function testEncode(): void
+    {
+        $data = [
+            $this->getRandomString('k1') => $this->getRandomInt(),
+            $this->getRandomString('k2') => null,
+            $this->getRandomString('k3') => $this->getRandomString(),
+        ];
+
+        $encoded = Json::encode($data);
+        Assert::assertIsString($encoded);
+        Assert::assertEquals($data, \json_decode($encoded, true));
+    }
+
+    /**
+     * Tests encoding of various data types.
+     */
+    public function testEncodeVariousTypes(): void
+    {
+        $data = [
+            'integer' => $this->getRandomInt(),
+            'string'  => $this->getRandomString(),
+            'null'    => null,
+            'boolean' => $this->getRandomBool(),
+            'float'   => $this->getRandomFloat(0, 100),
+            'array'   => [1, 2, 3],
+            'nested'  => [
+                $this->getRandomString() => $this->getRandomString(),
+                $this->getRandomString() => [
+                    $this->getRandomString() => $this->getRandomString(),
+                ],
+            ],
+        ];
+
+        $encoded = Json::encode($data);
+        Assert::assertIsString($encoded);
+        Assert::assertEquals($data, \json_decode($encoded, true));
+    }
+
+    /**
+     * Tests encoding of a large array to ensure it doesn't hit memory limits.
+     */
+    public function testEncodeLargeArray(): void
+    {
+        $data = [];
+        for ($i = 0; $i < 10000; $i++) {
+            $data[$this->getRandomString("key_{$i}")] = $this->getRandomString("value_{$i}");
+        }
+
+        $encoded = Json::encode($data);
+        Assert::assertIsString($encoded);
+        Assert::assertEquals($data, \json_decode($encoded, true));
+    }
+
+    /**
+     * Tests encoding of invalid UTF-8 strings.
+     */
+    public function testEncodeInvalidUtf8(): void
+    {
+        $invalidUtf8 = "\xB1\x31";
+        $data = ['invalid' => $invalidUtf8];
+
+        $this->expectException(\JsonException::class);
+        Json::encode($data);
+    }
+
 } // end of class
